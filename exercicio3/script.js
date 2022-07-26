7011d786b71dd1a20a525a6787b4f073303dada0
Juliana <juliana-marques-@hotmail.com>
2022-07-26 08:57:13 -0300

    exercício realizado

--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,1 +1,19 @@
-//Crie a const para a frase aqui
+//"Jorge tem uma casa verde e com portão azul, com os dizeres:" 
+//"BOAS VINDAS, mas não deixe o gato sair"
+
+
+//a
+const fraseAspas = "Jorge tem uma casa verde e com portão azul, com os dizeres: \"BOAS VINDAS, mas não deixe o gato sair\"."
+console.log(fraseAspas)
+
+//b
+const fraseTrocada = "Jorge tem uma casa verde e com portão azul, com os dizeres: \"BOAS VINDAS, mas não deixe o gato sair\".".replaceAll('verde','rosa').replaceAll('azul','laranja')
+
+//c)
+console.log(fraseTrocada)
+
+//EXTRA
+const frase1 = "Jorge tem uma casa verde e com portão azul, com os dizeres:"
+const frase2 = "\"BOAS VINDAS, mas não deixe o gato sair\".".toUpperCase()
+
+console.log(frase1,frase2)
